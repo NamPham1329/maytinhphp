c7b3461d703f4ea6a6d8ee597489a71b482c674e
NamPham <vannam1329@gmail.com>
2021-09-15 10:43:47 +0700

    change class

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 class caculator{
-    protected $str;
     protected $giatri1;
     protected $giatri2;
     protected $ketqua;
     protected $pheptinh;
-    function __construct($str)
+    function __construct($giatri1, $giatri2)
     {
-        $this->str=$str;
+        $this->giatri1 = $giatri1;
+        $this->giatri2 = $giatri2;
     }
     function cong()
     {
@@ -34,36 +34,43 @@ class caculator{
         $this->ketqua = $this->giatri1%$this->giatri2;
         return $this->ketqua;
     }
-    function chon(){
-        $this->pheptinh = ['+','-','x','÷','%'];
-        if(isset($_POST['group1'])){
-            foreach($this->pheptinh as $value){
-                if(strpos($_POST['domain'], $value)==true){
-                    $data = explode($value, $_POST['domain']);
-                    $this->giatri1 = (float)$data[0];
-                    $this->giatri2 = (float)$data[1];
-                    if($value == "+"){
-                        return $this->cong();
-                    }
-                    if($value == "-"){
-                        return $this->tru();
-                    }
-                    if($value == "x"){
-                        return $this->nhan();
-                    }
-                    if($value == "÷"){
-                        return $this->chia();
-                    }
-                    if($value == "%"){
-                        return $this->chia();
-                    }
-                }
-            }
+    function chon($pheptinh)
+    {
+        $this->pheptinh = $pheptinh;
+        switch($pheptinh){
+            case "+":
+                return $this->cong();
+                break;
+            case "-":
+                return $this->tru();
+                break;
+            case "x":
+                return $this->nhan();
+                break;
+            case "÷":
+                return $this->chia();
+                break;
+            case "%":
+                return $this->phandu();
+                break;   
         }
     }
 }
-$maytinh = new caculator($_POST['domain']);
-$ketqua =  $maytinh->chon();
+$operator = ['+','-','x','÷','%'];
+if(isset($_POST['group1']) && isset($_POST['domain'])){
+    $request = $_POST['group1'];
+    $content = $_POST['domain'];
+    foreach($operator as $value){
+        if(strpos($content, $value)==true){
+            $pheptinh = $value;
+            $data = explode($value, $_POST['domain']);
+            $giatri1 = (float)$data[0];
+            $giatri2 = (float)$data[1];
+        }
+    }
+}
+$maytinh = new caculator($giatri1, $giatri2);
+$ketqua =  $maytinh->chon($pheptinh);
 
 ?>
 <!DOCTYPE html>
